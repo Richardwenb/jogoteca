a47adc3beb5ce9eed4312c306653584badec6bc7
Richard Wendel <49850416+Richardwenb@users.noreply.github.com>
2023-03-28 11:50:06 -0300

    Initial commit

---
+++ jogoteca.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+jogo1 = Jogo('Tetris','Puzzle','Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+
+lista = [jogo1,jogo2,jogo3]
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html',titulo='Novo jogo')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome'] # recebendo dados do cliente e colocando em lista
+    categoria = request.form['categoria'] 
+    console = request.form['console'] 
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+app.run(debug=True)
